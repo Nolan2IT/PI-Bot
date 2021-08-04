b7a5d09e535cdada9f4363f418ecc55923cc75ac
Nolan2IT <tuitenol@berkeleyprep.org>
2021-08-04 00:57:10 -0400

    First Working Version

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import discord
 import os
 import random
@@ -22,34 +21,3 @@ async def get(ctx, title):
     prodInfo = fetcher.fetch(title)
     response = f'Average Cost: {prodInfo["avgCost"]}, Number of Listings: {prodInfo["count"]}'
     await ctx.send(response)
-
-=======
-import discord
-import os
-import random
-from dotenv import load_dotenv
-from discord.ext import commands
-
-bot = commands.Bot(command_prefix = '.')
-
-load_dotenv() #load env variables
-TOKEN = os.getenv('TOKEN') #gets token from .env file
-        
-@bot.event
-async def on_ready():
-    print("Bot Connected to the Server.")
-
-@bot.command()
-async def clear(ctx, amount = 5):
-    await ctx.channel.purge(limit=amount)
-
-@bot.command()
-async def kick(ctx, member : discord.Member, reason = None):
-    await member.kick(reason = reason)
-
-@bot.command()
-async def ban(ctx, member : discord.Member, reason = None):
-    await member.ban(reason = reason)
-
->>>>>>> 7caf6086e26c339b78c78fdd93df893811f2dc0e
-bot.run(TOKEN)
--- eBayFetcher.py
+++ eBayFetcher.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import datetime
 from dotenv import load_dotenv
@@ -39,42 +38,3 @@ class eBayFetcher(object):
 if __name__ == '__main__':
     e = eBayFetcher(TOKEN)
     e.fetch()
-=======
-import os
-import datetime
-from dotenv import load_dotenv
-from ebaysdk.exception import ConnectionError
-from ebaysdk.finding import Connection
-
-load_dotenv() #load env variables
-TOKEN = os.getenv('EBAY_TOKEN')
-
-class eBayFetcher(object):
-    def __init__(self, TOKEN):
-        self.token = TOKEN
-
-    def fetch(self):
-        try:
-            api = Connection(appid = self.token, config_file = None)
-            response = api.execute('findItemsAdvanced', {'keywords': 'legos'})
-
-            assert(response.reply.ack == 'Success')
-            assert(type(response.reply.timestamp) == datetime.datetime)
-            assert(type(response.reply.searchResult.item) == list)
-
-            item = response.reply.searchResult.item[0]
-            assert(type(item.listingInfo.endTime) == datetime.datetime)
-            assert(type(response.dict()) == dict)
-
-        except ConnectionError as e:
-            print(e)
-            print(e.response.dict())
-    
-    def parse(self):
-        pass
-
-if __name__ == '__main__':
-    e = eBayFetcher(TOKEN)
-    e.fetch()
->>>>>>> 7caf6086e26c339b78c78fdd93df893811f2dc0e
-    e.parse()
