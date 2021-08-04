0573807ff1bbd096440ee73315967cfffe59852a
Nolan2IT <tuitenol@berkeleyprep.org>
2021-08-04 00:50:25 -0400

    First Working Version

---
+++ bot.py
@@ -0,0 +1,25 @@
+import discord
+import os
+import random
+from dotenv import load_dotenv
+from discord.ext import commands
+from eBayFetcher import eBayFetcher
+
+fetcher = eBayFetcher()
+
+bot = commands.Bot(command_prefix = '.')
+
+load_dotenv() #load env variables
+TOKEN = os.getenv('TOKEN') #gets token from .env file
+
+@bot.event
+async def on_ready():
+    print("Bot Connected to the Server.")
+
+@bot.command()
+async def get(ctx, title):
+    prodInfo = fetcher.fetch(title)
+    response = f'Average Cost: {prodInfo["avgCost"]}, Number of Listings: {prodInfo["count"]}'
+    await ctx.send(response)
+
+bot.run(TOKEN)
---
+++ eBayFetcher.py
@@ -0,0 +1,41 @@
+import os
+import datetime
+from dotenv import load_dotenv
+from ebaysdk.exception import ConnectionError
+from ebaysdk.finding import Connection
+
+load_dotenv() #load env variables
+TOKEN = os.getenv('EBAY_TOKEN')
+
+class eBayFetcher(object):
+    def __init__(self):
+        self.api = Connection(appid = TOKEN, config_file = None)
+
+    def fetch(self, title = None): # , productCondition = None
+        try:
+            response = self.api.execute('findItemsAdvanced', {'keywords': title})
+            #print(response.reply)
+            avgCost = 0.0
+            count = 0
+            for item in response.reply.searchResult.item:
+                print(f"Title: {item.title}, Price {item.sellingStatus.currentPrice.value}")
+                #if "New" == productCondition:
+                avgCost += float(item.sellingStatus.currentPrice.value)
+                count += 1
+            avgCost /= count
+            prodInfo = {'avgCost' : f"{avgCost:.2f}", 'count' : count}
+            print(prodInfo)
+            return prodInfo    
+
+
+        except ConnectionError as e:
+            print(e)
+            print(e.response.dict())
+    
+    def parse(self):
+        pass
+
+if __name__ == '__main__':
+    e = eBayFetcher(TOKEN)
+    e.fetch()
+    e.parse()
